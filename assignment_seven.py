16acf3de39426fee5a3e9b8719a46988b7ab7bb5
CJ <cj@iMac7.local>
2018-11-01 10:13:31 -0400

    added an encode and got a choice

--- assignment_seven.py
+++ assignment_seven.py
@@ -1,21 +1,28 @@
 def choice():
-choice1 = input("press e to encode press d to decode or press q to quit:")
-if choice1 == "e":
-    print("encode")
-elif choice1 == "d":
-    print("decode")
-else:
-    print("Okay bye")
+    choice = input("press e to encode press d to decode or press q to quit:")
+    if choice == "e":
+        print("encode")
+    elif choice == "d":
+        print("decode")
+    else:
+        print("Okay bye")
 
 
 def main():
-    if choice1 == "e":
-        input("What would you like to encode?")
-    elif choice1 == "d":
-        input("What would you like to decode?")
-number = int(input("Choose a number between 0-25"))
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+    choice1 = int(input("Choose a number between 0-25"))
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    first = alphabet[0:choice1]
+    last = alphabet[choice1:]
+    final = first + last
+    encoded = ""
+    phrase = input("What would you like to encode")
+    
 
 
+
+        # if choice == "e":
+        #     input("What would you like to encode?")
+        # elif choice == "d":
+        #     input("What would you like to decode?"
 
 main()
