ca4f377d56a06e04145327b0547035e2695ba91e
CJ <cj@iMac7.local>
2018-11-02 11:21:01 -0400

    encoded and started to decode words

--- assignment_seven.py
+++ assignment_seven.py
@@ -1,28 +1,47 @@
 def choice():
     choice = input("press e to encode press d to decode or press q to quit:")
     if choice == "e":
-        print("encode")
+        return "e"
+
     elif choice == "d":
-        print("decode")
+        return "d"
     else:
         print("Okay bye")
 
 
 def main():
-    choice1 = int(input("Choose a number between 0-25"))
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    first = alphabet[0:choice1]
-    last = alphabet[choice1:]
-    final = first + last
-    encoded = ""
-    phrase = input("What would you like to encode")
-    
+    if choice == "e":
+        number = int(input("Choose a number between 0-25:"))
+        phrase = input("What would you like to encode?")
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        first = alphabet[0:number]
+        last = alphabet[number:]
+        final = last + first
+        encoded = ""
+        for x in phrase:
+            if x == " ":
+                encoded = encoded + " "
+            else:
+                code = alphabet.index(x)
+                final2 = final[code]
+                encoded = encoded + final2
+        print(encoded)
+    elif choice == "d":
+        number = int(input("Choose a number between 0-25:"))
+        phrase = input("What would you like to encode?")
+        alphabet = "zyxwvutsrqponmlkjihgfedcba"
+        first = alphabet[0:number]
+        last = alphabet[number:]
+        final = last + first
+        encoded = ""
+        for x in phrase:
+            if x == " ":
+                decoded = decoded + " "
+            else:
+                code = alphabet.index(x)
+                final2 = final[code]
+                decoded = decoded + final2
+        print(decoded)
 
-
-
-        # if choice == "e":
-        #     input("What would you like to encode?")
-        # elif choice == "d":
-        #     input("What would you like to decode?"
 
 main()
